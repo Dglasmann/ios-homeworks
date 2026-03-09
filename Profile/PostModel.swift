222fddecefa5d734c6f7dceeaff33e5e62af9bf9
dglasmann <dglasmann@mail.ru>
2026-03-09 16:34:10 +0700

    Homework 5

---
+++ Profile/PostModel.swift
@@ -0,0 +1,15 @@
+//
+//  Post.swift
+//  Navigation
+//
+//  Created by Sasha Soldatov on 09.03.2026.
+//
+
+
+struct PostModel {
+    let author: String
+    let description: String
+    let image: String
+    let likes: Int
+    let views: Int
+}
--- Profile/ProfileViewController.swift
+++ Profile/ProfileViewController.swift
@@ -8,53 +8,99 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
-    //MARK: - subviews
-    private let profileHeaderView: ProfileHeaderView = {
-        let view = ProfileHeaderView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
+    
+    //MARK: - Data
+    
+    private let posts: [PostModel] = [
+        PostModel(
+            author: "vedmak.official",
+            description: "Новые кадры со съемок второго сезона Ведьмаке.",
+            image: "post1",
+            likes: 240,
+            views: 560
+        ),
+        PostModel(
+            author: "Крутые уроки по Swift",
+            description: "А вы знали, что можно использовать guard let в Swift?",
+            image: "post2",
+            likes: 530,
+            views: 1245
+        ),
+        PostModel(
+            author: "java.qa",
+            description: "Как в 2026 можно использовать AI в тестировании?",
+            image: "post3",
+            likes: 13,
+            views: 553
+        ),
+        PostModel(
+            author: "nasa.exploring",
+            description: "Новые снимки с телескопа Джейма Уэбба показали новые планеты в Солнечной системе.",
+            image: "post4",
+            likes: 5840,
+            views: 13402
+        )
+    ]
+    
+    //MARK: - Subviews
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(PostTableViewCell.self, forCellReuseIdentifier: "PostTableViewCell")
+        return tableView
     }()
     
-    private lazy var bottomButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Какая то новая кнопка", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
-        button.backgroundColor = .systemBlue
-        button.layer.cornerRadius = 10
-        return button
-    }()
-    
-    //MARK: - lifecycle
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .lightGray
+        view.backgroundColor = .systemBackground
         title = "Profile"
-        setupView()
+        setupViews()
         setupConstraints()
     }
     
-    //MARK: - setup
-    private func setupView() {
-        view.addSubview(profileHeaderView)
-        view.addSubview(bottomButton)
+    private func setupViews() {
+        view.addSubview(tableView)
     }
     
     private func setupConstraints() {
-        
         NSLayoutConstraint.activate([
-        profileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-        profileHeaderView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-        profileHeaderView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-        profileHeaderView.heightAnchor.constraint(equalToConstant: 220),
-        
-        bottomButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-        bottomButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-        bottomButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-        bottomButton.heightAnchor.constraint(equalToConstant: 50),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
     }
+}
+
+//MARK: - Extensions
+extension ProfileViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+        
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->    UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PostTableViewCell", for:   indexPath) as? PostTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.configure(with: posts[indexPath.row])
+        return cell
+    }
+}
+
+//это для задания "Используйте ProfileTableHederView в качестве HeaderForSection для нулевой секции."
+//показываем наш ProfileHeaderView над таблицей в качестве заголовка
+extension ProfileViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard section == 0 else { return nil }
+        let headerView = ProfileHeaderView()
+        return headerView
+    }
     
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        guard section == 0 else { return 0 }
+        return 220
+    }
 }
