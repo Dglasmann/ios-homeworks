334420103442dec29cc6a093267f63aa4b941f1a
Dglasmann <37260677+Dglasmann@users.noreply.github.com>
2026-04-23 22:39:27 +0700

    Revert "hw2"

--- Navigation/Profile/PostTableViewCell.swift
+++ Navigation/Profile/PostTableViewCell.swift
@@ -7,8 +7,6 @@
 
 
 import UIKit
-import iOSIntPackage
-
 class PostTableViewCell: UITableViewCell {
     
     
@@ -115,17 +113,10 @@ class PostTableViewCell: UITableViewCell {
     
     
     func configure(with post: PostModel) {
-        authorLabel.text = post.author
-        descriptionLabel.text = post.description
-        likesLabel.text = "Likes: \(post.likes)"
-        viewsLabel.text = "Views: \(post.views)"
-        
-        guard let image = UIImage(named: post.image) else { return }
-        let processor = ImageProcessor()
-        processor.processImage(sourceImage: image, filter: .noir) { result in
-            DispatchQueue.main.async {
-                self.postImageView.image = result
-            }
-        }
+            authorLabel.text = post.author
+            postImageView.image = UIImage(named: post.image)
+            descriptionLabel.text = post.description
+            likesLabel.text = "Likes: \(post.likes)"
+            viewsLabel.text = "Views: \(post.views)"
         }
 }
