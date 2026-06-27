1436901412a73168d5f44675075f9bc7ec8cc599
dglasmann <dglasmann@mail.ru>
2026-06-27 16:53:30 +0700

    hw fixes

--- Navigation/Profile/FavouritesViewController.swift
+++ Navigation/Profile/FavouritesViewController.swift
@@ -24,6 +24,7 @@ final class FavouritesViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Избранное"
+        setupNavigationBar()
         setupViews()
     }
     
@@ -31,6 +32,23 @@ final class FavouritesViewController: UIViewController {
         super.viewWillAppear(animated)
         posts = CoreDataService.shared.fetchPosts()
         tableView.reloadData()
+    }
+    
+    private func setupNavigationBar() {
+        let filterButton = UIBarButtonItem(
+            image: UIImage(systemName: "magnifyingglass"),
+            style: .plain,
+            target: self,
+            action: #selector(showFilterAlert)
+        )
+        
+        let clearButton = UIBarButtonItem(
+            image: UIImage(systemName: "xmark.circle"),
+            style: .plain,
+            target: self,
+            action: #selector(clearFilter)
+        )
+        navigationItem.rightBarButtonItems = [clearButton, filterButton]
     }
     
     private func setupViews() {
@@ -42,9 +60,40 @@ final class FavouritesViewController: UIViewController {
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
+    
+    
+    @objc private func showFilterAlert() {
+        let alert = UIAlertController(
+            title: "Поиск по автору",
+            message: "Введите имя автора",
+            preferredStyle: .alert
+        )
+        alert.addTextField { textField in
+            textField.placeholder = "Автор"
+        }
+        
+        let applyAction = UIAlertAction(title: "Применить", style: .default) { [weak self, weak alert] _ in
+            guard let self = self else { return }
+            let author = alert?.textFields?.first?.text ?? ""
+            guard !author.isEmpty else { return }
+            self.posts = CoreDataService.shared.fetchPosts(author: author)
+            self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Отмена", style: .cancel)
+        alert.addAction(applyAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+    
+    @objc private func clearFilter() {
+        posts = CoreDataService.shared.fetchPosts()
+        tableView.reloadData()
+    }
 }
 
 extension FavouritesViewController: UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         posts.count
     }
@@ -53,6 +102,7 @@ extension FavouritesViewController: UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "PostTableViewCell", for: indexPath) as? PostTableViewCell else {
             return UITableViewCell()
         }
+        
         cell.configure(with: posts[indexPath.row])
         return cell
     }
@@ -60,11 +110,24 @@ extension FavouritesViewController: UITableViewDataSource {
 }
 
 extension FavouritesViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        guard editingStyle == .delete else { return }
-        let post = posts[indexPath.row]
-        posts.remove(at: indexPath.row)
-        tableView.deleteRows(at: [indexPath], with: .automatic)
-        CoreDataService.shared.deletePost(post)
+    func tableView(
+        _ tableView: UITableView,
+        trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath
+    ) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(
+            style: .destructive,
+            title: "Удалить"
+        ) { [weak self] _, _, completion in
+            guard let self = self else {
+                completion(false)
+                return
+            }
+            let post = self.posts[indexPath.row]
+            self.posts.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            CoreDataService.shared.deletePost(post)
+            completion(true)
+        }
+        return UISwipeActionsConfiguration(actions: [deleteAction])
     }
 }
